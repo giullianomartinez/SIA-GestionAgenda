d06e698df205ae8ff0e2cafd9b64e039d2ee5131
Giulliano Martínez <giulliano.martinez.y@gmail.com>
2026-09-03 14:07:59 -0400

    Replace MenuAgenda with MenuInicio in main method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SistemaAgenda sistema = new SistemaAgenda();
-        MenuAgenda menu = new MenuAgenda(sistema);
-
-        menu.seleccionarModo();
+        MenuInicio menu = new MenuInicio();
         menu.iniciar();
     }
 }
