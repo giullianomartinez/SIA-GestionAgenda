5078c048b1751d129afdb9a6d9017515a4c28f1f
Giulliano Martínez <giulliano.martinez.y@gmail.com>
2026-09-03 13:23:23 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         SistemaAgenda sistema = new SistemaAgenda();
         MenuAgenda menu = new MenuAgenda(sistema);
+
+        menu.seleccionarModo();
         menu.iniciar();
     }
 }
