d97979a3d33fdfba57ad08cebebfe647061f6f06
Giulliano Martínez <giulliano.martinez.y@gmail.com>
2026-09-07 19:06:22 -0300

    Refactor ActividadProyecto constructor and methods

--- src/ActividadProyecto.java
+++ src/ActividadProyecto.java
@@ -1,27 +1,60 @@
 import java.time.LocalTime;
 
-public class ActividadProyecto extends Actividad {
+public class ActividadProyecto
+        extends Actividad {
 
     private String nombreProyecto;
 
-    public ActividadProyecto(int id, String titulo, LocalTime horaInicio,
-                             LocalTime horaFin, String descripcion,
-                             String nombreProyecto) {
+    public ActividadProyecto(
+            int id,
+            String titulo,
+            LocalTime horaInicio,
+            LocalTime horaFin,
+            String descripcion,
+            String nombreProyecto) {
 
-        super(id, titulo, horaInicio, horaFin, descripcion, "Proyecto");
-        this.nombreProyecto = nombreProyecto;
+        super(
+                id,
+                titulo,
+                horaInicio,
+                horaFin,
+                descripcion,
+                "Proyecto"
+        );
+
+        this.nombreProyecto
+                = nombreProyecto;
     }
 
     public String getNombreProyecto() {
         return nombreProyecto;
     }
 
-    public void setNombreProyecto(String nombreProyecto) {
-        this.nombreProyecto = nombreProyecto;
+    public void setNombreProyecto(
+            String nombreProyecto) {
+
+        this.nombreProyecto
+                = nombreProyecto;
+    }
+
+    @Override
+    public String getNombreDatoEspecifico() {
+
+        return "nombre del proyecto";
+    }
+
+    @Override
+    public void setDatoEspecifico(
+            String dato) {
+
+        setNombreProyecto(dato);
     }
 
     @Override
     public String mostrarActividad() {
-        return super.mostrarActividad() + " | Proyecto: " + nombreProyecto;
+
+        return super.mostrarActividad()
+                + " | Proyecto: "
+                + nombreProyecto;
     }
 }
